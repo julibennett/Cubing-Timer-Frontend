fe80c31bece3cb4ae9b347ca68f82e46b6db08a5
julibennett <julibennett4@gmail.com>
2024-05-26 16:42:28 -0700

    fix environmental variables

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
-import axios from 'axios'
-import { ACCESS_TOKEN } from './constants'
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
 
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL
+});
 
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN)
+        const token = localStorage.getItem(ACCESS_TOKEN);
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+        return config;
     },
     (error) => {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
-)
+);
 
-export default api
+export default api;
