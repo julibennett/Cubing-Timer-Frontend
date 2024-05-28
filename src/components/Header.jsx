50c9e9c74d0c22e0ad08c93bdc2709d92d69f8ae
julibennett <julibennett4@gmail.com>
2024-05-28 01:25:45 -0700

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,41 +13,46 @@ const Header = () => {
 
   return (
     <header className="bg-black p-4 flex justify-between items-center sticky top-0 z-50">
-      <nav className="flex flex-1 space-x-4 justify-end">
-        {username ? (
-          <>
+      {username ? (
+        <>
+          <nav className="flex flex-1 space-x-4 justify-end">
             <span className="text-white wheaton">Welcome, {username}</span>
-            <Link to="/chart" className="text-white hover:text-gray-300 wheaton">
-              Charts
-            </Link>
-            <Link to="/search-users" className="text-white hover:text-gray-300 wheaton">
-              Search Users
-            </Link>
             <button
               onClick={handleLogout}
               className="flex items-center bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
             >
               <img src="/icons/log-out.png" alt="Logout" className="w-4 h-4" />
             </button>
-          </>
-        ) : (
-          <>
+          </nav>
+          <Link to="/" className="flex-1 text-white text-6xl font-bold headfont text-center">
+            R U R' U'
+          </Link>
+          <nav className="flex flex-1 space-x-4 justify-start">
+            <Link to="/chart" className="text-white hover:text-gray-300 wheaton">
+              Charts
+            </Link>
+            <Link to="/search-users" className="text-white hover:text-gray-300 wheaton">
+              Search Users
+            </Link>
+          </nav>
+        </>
+      ) : (
+        <>
+          <nav className="flex flex-1 space-x-4 justify-end">
             <Link to="/login" className="text-white hover:text-gray-300 wheaton">
               Login
             </Link>
-          </>
-        )}
-      </nav>
-      <Link to="/" className="flex-1 text-white text-6xl font-bold headfont text-center">
-        R U R' U'
-      </Link>
-      <nav className="flex flex-1 space-x-4 justify-start">
-        {!username && (
-          <Link to="/register" className="text-white hover:text-gray-300 wheaton">
-            Register
+          </nav>
+          <Link to="/" className="flex-1 text-white text-6xl font-bold headfont text-center">
+            R U R' U'
           </Link>
-        )}
-      </nav>
+          <nav className="flex flex-1 space-x-4 justify-start">
+            <Link to="/register" className="text-white hover:text-gray-300 wheaton">
+              Register
+            </Link>
+          </nav>
+        </>
+      )}
     </header>
   );
 };
