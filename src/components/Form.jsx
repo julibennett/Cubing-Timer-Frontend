29dad74a19ce15ab2c50bb2422714091d04d4fc5
julibennett <julibennett4@gmail.com>
2024-05-27 22:21:42 -0700

    refactoring

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,7 +23,7 @@ function Form({ route, method }) {
       if (method === 'login') {
         localStorage.setItem(ACCESS_TOKEN, res.data.access);
         localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-        localStorage.setItem('username', username); 
+        localStorage.setItem('username', username);
         console.log('Stored username:', localStorage.getItem('username'));
         navigate('/');
       } else {
